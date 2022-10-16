9752bc6d1134df7c3b4d687822d8d48b8df98743
“Salome <salomegathumbi@gmail.com>
2022-10-16 13:33:39 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase and in uppercase  
+ * main - prints the alphabet in lowercase and in uppercase
  * Return: Always 0 (Success)
  */
 
