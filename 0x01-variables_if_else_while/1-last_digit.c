995b5022ae708321b7445ec1ee6be64d60cc4124
“Salome <salomegathumbi@gmail.com>
2022-10-16 13:00:15 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,10 +17,10 @@ int main(void)
 	ld = n % 10;
 
 	if (ld > 5)
-		printf("%s %d is %d and is greater than 5\n", str, n, ld);
+		printf("%s %i is %i and is greater than 5\n", str, n, ld);
 	else if (ld == 0)
-		printf("%s %d is %d and is 0\n", str, n, ld);
+		printf("%s %i is %i and is 0\n", str, n, ld);
 	else if (ld < 6)
-		printf("%s %d is %d and is less than 6 and not 0\n", n, ld);
+		printf("%s %i is %i and is less than 6 and not 0\n", n, ld);
 	return (0);
 }
