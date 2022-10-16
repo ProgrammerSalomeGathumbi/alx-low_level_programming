5279fa2a169d3941c6e33e26c26efaa5e857c38f
“Salome <salomegathumbi@gmail.com>
2022-10-16 14:09:52 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - rints all single digit numbers of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; a < 10; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
