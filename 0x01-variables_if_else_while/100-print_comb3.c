3ae5cc4d767f124eae67f3bfea71785e8da5ba43
“Salome <salomegathumbi@gmail.com>
2022-10-17 06:00:54 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints different combinations of two digits.
+ * Return: Always 0 (Success0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (a = b + 1; b < 10; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+			putchar(',');
+			putchar(' ');
+
+			if (a == 8 && b == 9)
+				continue;
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
