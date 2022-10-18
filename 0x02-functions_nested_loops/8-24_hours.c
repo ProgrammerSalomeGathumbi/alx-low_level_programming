be0b72b4301c89ac88f98cd88684331505c3105e
“Salome <salomegathumbi@gmail.com>
2022-10-18 16:34:33 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,15 +9,15 @@ void jack_bauer(void)
 	int h, m;
 
 	for (h = 0; h < 24; h++)
+	{
+		for (m = 00; m < 60; m++)
 		{
-			for (m = 00; m < 60; m++)
-				{
-					_putchar(h/10 + '0');
-					_putchar(h%10 + '0');
-					_putchar(':');
-					_putchar(m/10 + '0');
-					_putchar(m%10 + '0');
-					_putchar('\n');
-				}
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
 		}
+	}
 }
