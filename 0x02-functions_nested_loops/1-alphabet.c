826b17336d77252f32ebf5ba3012ab302f9792e0
“Salome <salomegathumbi@gmail.com>
2022-10-18 05:29:06 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: Always 0.
+ */
+
+void print_alphabet(void);
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+putchar('a');
+putchar('\n');
+return (0);
+}
