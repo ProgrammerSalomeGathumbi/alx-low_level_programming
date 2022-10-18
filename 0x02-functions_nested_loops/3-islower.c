c181683475d64bce2f24b2bdc045704e3bc7fdc9
“Salome <salomegathumbi@gmail.com>
2022-10-18 12:20:14 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * _islower - checks for lowercase character
+ * @c: single character input
  * Return: Always 0
  */
 int _islower(int c)
 {
-	if (_islower(c))
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	return (0);
 }
