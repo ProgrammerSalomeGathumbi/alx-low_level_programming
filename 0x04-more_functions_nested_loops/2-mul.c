f8a243db7dac14c3d3031513b35e2c26cec287b6
“Salome <salomegathumbi@gmail.com>
2022-10-21 09:58:40 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * mul -multiplies two integers.
+  * @a: integer input
+  * @b: integer input
+  * Return:  Always 0
+  */
+int mul(int a, int b)
+{
+	int c;
+	c = (a * b);
+
+	_putchar(c);
+	_putchar('\n');
+	return (0);
+}
