a00fcc7fb7b1355cebd6d0e4d096780566402b19
“Salome <salomegathumbi@gmail.com>
2022-10-25 05:38:05 -0700

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swaps value of two integers
+ * @*a: pointer to integer
+ * @*b: pointer to integer
+ * @a: integer input
+ * @b: integer input
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	*a = *b;
+	c = *a;
+	*b = c;
+}
