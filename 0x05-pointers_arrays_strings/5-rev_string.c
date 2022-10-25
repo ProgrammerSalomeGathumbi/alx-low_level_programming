22918fa83705c8c67228fc808a1e3e939bf90470
“Salome <salomegathumbi@gmail.com>
2022-10-25 08:25:36 -0700

    5-print_rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - print string in reverse
+ * @*s: pointer to character
+ * @s: charater input
+ * return:reverse s
+ */
+ void rev_string(char *s)
+{
+	int i, j;
+	int a = 0;
+
+	while (*(s + a) != '\0')
+	{
+		a++;
+	}
+	a -= 1;
+	i = 0;
+	while (a > i)
+	{
+		j = s[i];
+		s[i] = s[a];
+		s[a] = j;
+		i++;
+		a--;
+	}
+}
