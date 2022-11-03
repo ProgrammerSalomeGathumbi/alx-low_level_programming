36e41a26e58da3bf2930ffbf141a5607550cd59d
“Salome <salomegathumbi@gmail.com>
2022-11-03 07:19:27 -0700

    3-factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * factorial - returns factorial of a number
+  * @n: integer
+  *Return: -1 to indicate error
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+		return (n * factorial(n - 1));
+}
