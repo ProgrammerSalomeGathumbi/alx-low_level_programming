756cc8a710d93f89c6e36d8db65527e21128b699
“Salome <salomegathumbi@gmail.com>
2022-11-07 03:28:10 -0800

    Hot fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <ctype.h>
 /**
  * main - Entry Point
  * @argc: arguments
@@ -9,20 +10,27 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, c, sum = 0;
 
-	if (argc < 1)
+	if (argc < 2)
+	{
+		printf("0\n");
 		return (0);
+	}
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
+		for (c = 0; argv[i][c] != '\0'; c++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+			if (!isdigit(argv[i][c]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		sum += atoi(argv[i]);
 	}
+
 	printf("%d\n", sum);
 	return (0);
 }
