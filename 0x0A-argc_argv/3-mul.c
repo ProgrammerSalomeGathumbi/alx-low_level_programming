babe1c58b506179d109e844886b4cbf009c1eb56
“Salome <salomegathumbi@gmail.com>
2022-11-07 00:00:01 -0800

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+ #include <stdio.h>
+#include <stdlib.h>
+/**
+  * main -Entry point
+  * @argc: counts arguments
+  * @argv: string containing arguments
+  * Return: 1 when there is error
+  */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int j;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (argc > 1)
+	{
+		j = atoi(argv[i]) * atoi(argv[i + 1]);
+		printf("%d\n", j);
+	}
+	return (0);
+}
