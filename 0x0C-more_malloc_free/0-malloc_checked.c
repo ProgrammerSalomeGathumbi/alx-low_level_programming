b52267a988f006417fd22f2623322611605aa6c1
“Salome <salomegathumbi@gmail.com>
2022-11-11 05:50:19 -0800

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: unsigned int
+ * Return: nothing 
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *n;
+
+	n = malloc(b);
+
+	if (n == NULL)
+	{
+		exit(98);
+	}
+
+	return (n);
+}
