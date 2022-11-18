09ab540f0ad2123596c9a3ef50bee200c0c45ffb
“Salome <salomegathumbi@gmail.com>
2022-11-18 06:23:36 -0800

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ *  print_numbers -  prints numbers
+ *  @separator: String between numbers
+ *  @n: Number of args
+ *  Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, unsigned int));
+
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	putchar('\n');
+
+	va_end(ap);
+}
