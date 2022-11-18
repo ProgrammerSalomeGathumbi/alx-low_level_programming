2572b3212ec1d1c6135ba8e31096de9c2fa428d3
“Salome <salomegathumbi@gmail.com>
2022-11-18 08:10:42 -0800

    3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,15 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-
+/**
+ * struct print_form - Define a struct to figure out how to print
+ * @c: Character to check for
+ * @f: Pointer to function to call
+ */
+typedef struct print_form
+{
+	char *c;
+	void (*f)();
+} print_form_t;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
