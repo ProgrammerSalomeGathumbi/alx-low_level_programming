9fdaf13ac906b5ba6097864e998b7ff4cda34df4
“Salome <salomegathumbi@gmail.com>
2022-11-28 10:57:37 -0800

    fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,15 +10,23 @@
   */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	int i = 0;
+	list_t *new_node;
 
-	new_node->str = strdup(str);
-	new_node->len = strlen(str);
-	new_node->next = (*head);
-	(*head) = new_node;
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
-	else
-		return (*head);
+	while (str[i] != '\0')
+		i++;
+
+	new_node->str = strdup(str);
+	new_node->len = i;
+	new_node->next = (*head);
+	(*head) = new_node;
+
+	return (*head);
 }
